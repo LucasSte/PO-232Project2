09f6057bcce6bb6cdc8472266518e1cc2e923a3c
LucasSte <lucas.tnagel@gmail.com>
2021-05-07 12:18:39 -0300

    Add exemple graphs

--- main.cpp
+++ main.cpp
@@ -4,17 +4,16 @@
 int main() {
     Parser p;
     int maxSize = 0;
-    p.readEdgesFromFile(&maxSize, "../g1.dat");
+    p.readEdgesFromFile(&maxSize, "../g3.dat");
     auto ret = p.getEdges();
 
     Graph g(maxSize);
     g.buildGraph(ret);
-    auto ret2 = g.getUniqueEdgeList();
-    p.printGraph("../out1.dat", ret2, "GL");
 
     vector<Graph> trees = g.primAlgorithm();
 
     for(int i=0; i<trees.size(); i++) {
+
         p.printGraph("../tree" + to_string(i) + ".dat", trees[i].getUniqueEdgeList(), "Tree" + to_string(i));
     }
 
